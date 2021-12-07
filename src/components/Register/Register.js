019326bf69c394018a75707cd9abb1bc1c7c16e2
Divyansh Pandey <divyanshpandey99@gmail.com>
2021-12-07 16:54:50 +0530

    Server compatible

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends Component{
     }
     
     onSubmitSignIn = ()=>{
-        fetch('http://localhost:3000/register', {
+        fetch('https://morning-island-99299.herokuapp.com/register', {
             method:'post',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -20,7 +20,7 @@ class Signin extends Component{
     }
 
     onSubmitSignIn = ()=>{
-        fetch('http://localhost:3000/signin', {
+        fetch('https://morning-island-99299.herokuapp.com/signin', {
             method:'post',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
